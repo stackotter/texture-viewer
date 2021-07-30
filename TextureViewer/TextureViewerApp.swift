6d4a753b94ee9ae4fd65c81ad1ad2340401e98c2
stackotter <rohanarievk@gmail.com>
2021-07-30 21:54:21 +1000

    Initial Commit

---
+++ TextureViewer/TextureViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextureViewerApp.swift
+//  TextureViewer
+//
+//  Created by Rohan van Klinken on 30/7/21.
+//
+
+import SwiftUI
+
+@main
+struct TextureViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
